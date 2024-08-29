91061644e1fec07292b49cf1c1ad632c54a95b39
tuanvm <tuanvm@vnext.vn>
2024-08-29 17:28:08 +0700

    create client

---
+++ resources/views/user/news.blade.php
@@ -0,0 +1,30 @@
+@extends('user.main')
+@section('templateContent')
+
+<main class="mb-5">
+        <div class="head_page">
+            <div class="container">
+
+                <ol vocab="https://schema.org/" typeof="BreadcrumbList" class="breadcrumb">
+                    <li property="itemListElement" typeof="ListItem" class="breadcrumb-item">
+                        <a property="item" typeof="WebPage" href="index.html">
+                            <span property="name">Trang chủ</span></a>
+                        <meta property="position" content="1">
+                    </li>
+
+                    <li property="itemListElement" typeof="ListItem" class="breadcrumb-item active">
+                        <a property="item" typeof="WebPage" href="tin-tuc.html">
+                            <span property="name">Tin tức</span></a>
+                        <meta property="position" content="2">
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row g-3 row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-6" id="result">
+            </div>
+        </div>
+        <script src="templates/js/jquery.twbsPagination.js"></script>
+    </main>
+@endsection
